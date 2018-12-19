115e1747947099ebee658d75a7e9f584f850e0c0
Harlan Evans <harlanevans5@gmail.com>
2018-12-18 17:42:42 -0700

    delete function

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { connect, } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Button, Icon, Container, Header, Item } from 'semantic-ui-react';
+import { deleteBlog, } from "../reducers/blogs";
+import { Button, Icon, Container, Header, } from 'semantic-ui-react';
+
+class Blog extends React.Component {
+  state = { showForm: false, };
 
 
-const Blog = ({ blog = {} }) => (
-  <Container>
-           <Header as='h1'
+  handleDelete = () => {
+    const { blog, dispatch, history: { push, }, } = this.props;
+    dispatch(deleteBlog(blog.id));
+    push("/blogs");
+  }
+
+  render() {
+    const { blog = {}, } = this.props;
+
+    return (
+
+      <Container>
+        <Header as='h1'
           style={{
             display: 'flex',
             justifyContent: 'center',
@@ -14,35 +28,58 @@ const Blog = ({ blog = {} }) => (
             fontFamily: '"Charm", cursive'
           }}>
           {blog.name}
-          </Header>
-          <hr />
-          <br />
-          <p
+        </Header>
+        <hr />
+        <br />
+        <p
           style={{
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
             fontFamily: "Charm",
           }}>
-            {blog.body}
-          </p>
-          <br />
+          {blog.body}
+        </p>
+        <br />
+        <div style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}>
+
           <Link to="/blogs">
-          <Button 
-          basic
-          color='black'
-          style={{fontFamily: "Charm",}} 
-          animated='fade'>
-           <Button.Content visible>
-           Back to All Blogs
+            <Button
+              basic
+              color='black'
+              style={{ fontFamily: "Charm", }}
+              animated='fade'>
+              <Button.Content visible>
+                Back to All Blogs
            </Button.Content>
-           <Button.Content hidden>
-           <Icon name='left arrow' />
-           </Button.Content>
-           </Button>
+              <Button.Content hidden>
+                <Icon name='left arrow' />
+              </Button.Content>
+            </Button>
           </Link>
-  </Container>
-)
+
+          <Button onClick={this.handleDelete}
+            alignItems='right'
+            basic
+            color='black'
+            style={{ fontFamily: "Charm", }}
+            animated='fade'>
+            <Button.Content visible>
+              Delete Blog
+           </Button.Content>
+            <Button.Content hidden>
+              <Icon name='delete' />
+            </Button.Content>
+          </Button>
+        </div>
+      </Container>
+    )
+  }
+}
+
 
 const mapStateToProps = (store, props) => {
   return { blog: store.blogs.find(a => a.id === parseInt(props.match.params.id)) }
